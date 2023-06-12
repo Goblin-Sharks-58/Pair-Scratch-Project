eaac88d3e6051a04b680ebd23ca6bb93087c861d
Philip Brown <philip.murga@gmail.com>
2023-06-12 14:31:24 -0700

    Updated myprofile

--- client/components/MyProfile.jsx
+++ client/components/MyProfile.jsx
@@ -1,18 +1,17 @@
 //Component to pull up another person's profile for review. Accept button. Reject button.
 import React, { useState } from 'react';
-import {useLocation} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const MyProfile = () => {
-  const userID = 1;
   const myLocation = useLocation();
   const resultObj = {};
-  fetch('http://localhost:8080', {
+  fetch('http://localhost:8080/myprofile', {
     headers: {
       // 'Accept': 'application.json',
       'Content-Type': 'application/json'
       //'Content-Type':'application/x-www-form-urlencoded'
     },
-    body: JSON.stringify(userID),
+    body: JSON.stringify(myLocation.state.response.userID),
   })
     .then(response => {
       response.json();
